eaf35676ea17683d85059f1cde6bbc0d46703a3e
PecchiniWellington <wellingtonpecchini@gmail.com>
2025-01-13 18:17:18 +0100

    Add AuthRoute

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from "@/components/navigation/navbar";
+
 export default function Home() {
   return (
     <>
+      <Navbar />
       <h1 className="text-3xl">Benvenuto in Miopetit social</h1>
     </>
   );
