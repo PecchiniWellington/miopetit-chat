d1f81f252e64ee92697c302818bb3f06c1619096
PecchiniWellington <wellingtonpecchini@gmail.com>
2025-01-16 14:35:48 +0100

    Create Question form

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -1,9 +1,14 @@
 "use client";
 
-import { AskQuestionSchema } from "@/lib/validations";
 import { zodResolver } from "@hookform/resolvers/zod";
-import React from "react";
-import { Path, useForm } from "react-hook-form";
+import { MDXEditorMethods } from "@mdxeditor/editor";
+import dynamic from "next/dynamic";
+import React, { useRef } from "react";
+import { useForm } from "react-hook-form";
+
+import { AskQuestionSchema } from "@/lib/validations";
+
+import { Button } from "../ui/button";
 import {
   Form,
   FormControl,
@@ -14,9 +19,14 @@ import {
   FormMessage,
 } from "../ui/form";
 import { Input } from "../ui/input";
-import { Button } from "../ui/button";
+
+const Editor = dynamic(() => import("@/components/editor"), {
+  ssr: false,
+});
 
 const QuestionForm = () => {
+  const editorRef = useRef<MDXEditorMethods>(null);
+
   const form = useForm({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
@@ -31,26 +41,25 @@ const QuestionForm = () => {
   return (
     <Form {...form}>
       <form
-        action=""
-        className="flex w.full flex-col gap-10"
+        className="flex w-full flex-col gap-10"
         onSubmit={form.handleSubmit(handleCreateQuestion)}
       >
         <FormField
           control={form.control}
           name="title"
           render={({ field }) => (
-            <FormItem className="flex-col flex w-full gap-2.5">
-              <FormLabel className="paragraph-semibold  text-dark400_light800">
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
                 Question Title <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl>
                 <Input
-                  className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 border no-focus min-h-[56px]"
+                  className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
                   {...field}
                 />
               </FormControl>
-              <FormDescription className="body-regular text-light-500 mt-2.5">
-                Be specific end Imagine you're asking a question to another
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Be specific and imagine you&apos;re asking a question to another
                 person.
               </FormDescription>
               <FormMessage />
@@ -61,14 +70,21 @@ const QuestionForm = () => {
           control={form.control}
           name="content"
           render={({ field }) => (
-            <FormItem className="flex-col flex w-full gap-2.5">
-              <FormLabel className="paragraph-semibold  text-dark400_light800">
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
                 Detailed explanation of your problem{" "}
                 <span className="text-primary-500">*</span>
               </FormLabel>
-              <FormControl>Editor</FormControl>
-              <FormDescription className="body-regular text-light-500 mt-2.5">
-                Introduce the problem and expand on what you've put in the title
+              <FormControl>
+                <Editor
+                  value={field.value}
+                  editorRef={editorRef}
+                  fieldChange={field.onChange}
+                />
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Introduce the problem and expand on what you&apos;ve put in the
+                title.
               </FormDescription>
               <FormMessage />
             </FormItem>
@@ -76,34 +92,35 @@ const QuestionForm = () => {
         />
         <FormField
           control={form.control}
-          name="title"
+          name="tags"
           render={({ field }) => (
-            <FormItem className="flex-col flex w-full gap-3">
-              <FormLabel className="paragraph-semibold  text-dark400_light800">
+            <FormItem className="flex w-full flex-col gap-3">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
                 Tags <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl>
                 <div>
                   <Input
-                    className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 border no-focus min-h-[56px]"
-                    {...field}
+                    className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
                     placeholder="Add tags..."
+                    {...field}
                   />
                   Tags
                 </div>
               </FormControl>
-              <FormDescription className="body-regular text-light-500 mt-2.5">
-                Add up 3 tags to describe what your question is about. You need
-                to press enter to add a tag.
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Add up to 3 tags to describe what your question is about. You
+                need to press enter to add a tag.
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
+
         <div className="mt-16 flex justify-end">
           <Button
-            className="primary-gradient !text-light-900 w-fit"
             type="submit"
+            className="primary-gradient w-fit !text-light-900"
           >
             Ask A Question
           </Button>
